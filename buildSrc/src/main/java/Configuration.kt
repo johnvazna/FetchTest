88cb71118534e58fc4e4068b5e49ebf9e6eca35e
John <johnvazna@gmail.com>
2023-08-14 19:21:10 -0600

    Add new module to handling all the dependencies and configuration app

---
+++ buildSrc/src/main/java/Configuration.kt
@@ -0,0 +1,10 @@
+/** */
+object Configuration {
+
+    const val versionCode = 1
+    const val minSdkVersion = 24
+    const val versionName = "1.0"
+    const val targetSdkVersion = 33
+    const val compileSdkVersion = 33
+    const val packageApp = "com.johnvazna.fetchtest"
+}
